ce0180066318dc04978c909da6766db78bf2a585
mak2220 <113451174+mak2220@users.noreply.github.com>
2022-12-29 18:51:38 -0300

    Add files via upload

---
+++ ejercicio_02.js
@@ -0,0 +1,10 @@
+const libro = {
+    titulo : "Cien años de soledad",
+    autor: "Gabriel Garcia Marquez",
+    edición: 1970,
+    "Pagina-Wed": "cienañosdesoledad.com"
+
+}
+
+const array = ["marcos", 39, true, fecha_nacimiento = new Date("september 19 1983"),libro];
+
